81f13031d3712088a91c33c079d4e299fbc697c2
Kenny Tye <ktye1217@gmail.com>
2021-10-06 11:49:26 -0700

    added a filter button to icon list

--- src/IconList.js
+++ src/IconList.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core';
-import icons from './icons';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import IconCard from './IconCard';
+import icons from './icons';
+
+const ITEM_HEIGHT = 48;
 
 const styles = {
   root: {
@@ -30,18 +38,94 @@ const styles = {
   },
 };
 
+const getIconNames = (iconList) => {
+  let filters = ['Clear Filter'];
+  iconList.forEach(icon => {
+    if (!filters.includes(icon.filter)) {
+      filters.push(icon.filter);
+    }
+  });
+  return filters;
+};
+
+const options = getIconNames(icons);
+
 function IconList(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [selectedFilter, setSelectedFilter] = useState('');
+  const open = Boolean(anchorEl);
   const { classes } = props;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (e) => {
+    if (e.target.role === 'menuitem') {
+      if (e.target.innerText === 'Clear Filter') {
+        setSelectedFilter('');
+      } else {
+        setSelectedFilter(e.target.innerText);
+      }
+    }
+    setAnchorEl(null);
+  };
   return (
     <div className={classes.IconListWrapper}>
+      <div>
+        <Button
+          sx={{
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            '&:hover': {
+              backgroundColor: 'rgba(7, 177, 77, 1)',
+            },
+          }}
+          id="demo-customized-button"
+          aria-controls="demo-customized-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          variant="contained"
+          disableElevation
+          onClick={handleClick}
+          endIcon={<KeyboardArrowDownIcon />}
+        >
+          Filter
+        </Button>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            'aria-labelledby': 'long-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: '20ch',
+            },
+          }}
+        >
+          {options.map((option) => (
+            <MenuItem key={option} selected={option === selectedFilter} onClick={handleClose}>
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
       <div className={classes.IconListCard}>
-        {icons.map(icon => (
-          <IconCard
-            iconClass={icon.class}
-            iconName={icon.name}
-            iconFilter={icon.filter}
-          />
-        ))}
+        {icons.map(icon => {
+          let filtered;
+          if (icon.filter === selectedFilter || selectedFilter === '') {
+            filtered = (
+              <IconCard
+                key={icon.name}
+                iconClass={icon.class}
+                iconName={icon.name}
+                iconFilter={icon.filter}
+              />
+            );
+          }
+          return filtered;
+        })}
       </div>
     </div>
   );
