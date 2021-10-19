569ad97525b3cd34541b4ba454e8768d4f7a1164
Kenny Tye <ktye1217@gmail.com>
2021-10-18 23:32:22 -0700

    fine tuned the sort filter and added styles to EditDeckList

--- src/EditDeckList.js
+++ src/EditDeckList.js
@@ -10,11 +10,13 @@ import Button from '@mui/material/Button';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Divider from '@mui/material/Divider';
 import EditDeckListItem from './EditDeckListItem';
 import IconCard from './IconCard';
 import icons from './icons';
 
 const ITEM_HEIGHT = 48;
+const ITEM_WIDTH = 1050;
 
 const options = ['Set Name', 'Sub Category', 'Category', 'Total Cards'];
 
@@ -27,6 +29,24 @@ const styles = {
     width: '100%',
     height: '100%',
     margin: '64px 0px 0px 0px',
+  },
+  menuBar: {
+    // border: '1px solid black',
+    height: '50px',
+    width: ITEM_WIDTH,
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  sortWrapper: {
+    marginLeft: 'auto',
+  },
+  divider: {
+    height: '1px',
+    width: ITEM_WIDTH,
+    borderBottom: '1px solid rgba(0, 0, 0, 0.2)',
+    margin: '0px 0px 15px 0px',
+    boxShadow: '0px 1px 3px 0px rgba(0, 0, 0, 0.1)',
   },
 };
 
@@ -95,6 +115,73 @@ function EditDeckList(props) {
   if (sortState.isSorted) {
     return (
       <div className={classes.EditDeckList}>
+        <div className={classes.menuBar}>
+          <h2>Edit Decks</h2>
+          <div className={classes.sortWrapper}>
+            <Button
+              sx={{
+                backgroundColor: 'rgba(250, 250, 250, 0.0)',
+                color: 'rgba(0, 0, 0, 0.6)',
+                height: '35px',
+                '&:hover': {
+                  backgroundColor: 'rgba(250, 250, 250, 0.0)',
+                  color: 'rgba(0, 0, 0, 0.8)',
+                },
+              }}
+              id="demo-customized-button"
+              aria-controls="demo-customized-menu"
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              variant="contained"
+              disableElevation
+              onClick={handleClick}
+              endIcon={<KeyboardArrowDownIcon />}
+            >
+              Sort
+            </Button>
+            <Menu
+              id="long-menu"
+              MenuListProps={{
+                'aria-labelledby': 'long-button',
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  maxHeight: ITEM_HEIGHT * 5.5,
+                  width: '20ch',
+                },
+              }}
+            >
+              <MenuItem onClick={handleClose}>Clear Filter</MenuItem>
+              {options.map((option) => (
+                <MenuItem key={option} selected={option === selectedFilter} onClick={handleClose}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </div>
+        <div className={classes.divider} />
+        {sortState.sortedDatabase.map(userCardSet => (
+          <EditDeckListItem
+            key={userCardSet.id}
+            category={userCardSet.category}
+            subCategory={userCardSet.subCategory}
+            iconClass={userCardSet.subCategoryClass}
+            setName={userCardSet.setName}
+            totalCards={userCardSet.cards.length}
+            url={`/my-collections/${userCardSet.subCategoryId}`}
+          />
+        ))}
+      </div>
+    );
+  }
+  return (
+    <div className={classes.EditDeckList}>
+      <div className={classes.menuBar}>
+        <h2>Edit Decks</h2>
         <div className={classes.sortWrapper}>
           <Button
             sx={{
@@ -140,68 +227,8 @@ function EditDeckList(props) {
             ))}
           </Menu>
         </div>
-        {sortState.sortedDatabase.map(userCardSet => (
-          <EditDeckListItem
-            key={userCardSet.id}
-            category={userCardSet.category}
-            subCategory={userCardSet.subCategory}
-            iconClass={userCardSet.subCategoryClass}
-            setName={userCardSet.setName}
-            totalCards={userCardSet.cards.length}
-            url={`/my-collections/${userCardSet.subCategoryId}`}
-          />
-        ))}
-        <button type="button" onClick={sortByCategory}>Sort</button>
       </div>
-    );
-  }
-  return (
-    <div className={classes.EditDeckList}>
-      <div className={classes.sortWrapper}>
-        <Button
-          sx={{
-            backgroundColor: 'rgba(250, 250, 250, 0.0)',
-            color: 'rgba(0, 0, 0, 0.6)',
-            height: '35px',
-            '&:hover': {
-              backgroundColor: 'rgba(250, 250, 250, 0.0)',
-              color: 'rgba(0, 0, 0, 0.8)',
-            },
-          }}
-          id="demo-customized-button"
-          aria-controls="demo-customized-menu"
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          variant="contained"
-          disableElevation
-          onClick={handleClick}
-          endIcon={<KeyboardArrowDownIcon />}
-        >
-          Sort
-        </Button>
-        <Menu
-          id="long-menu"
-          MenuListProps={{
-            'aria-labelledby': 'long-button',
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              maxHeight: ITEM_HEIGHT * 5.5,
-              width: '20ch',
-            },
-          }}
-        >
-          <MenuItem onClick={handleClose}>Clear Filter</MenuItem>
-          {options.map((option) => (
-            <MenuItem key={option} selected={option === selectedFilter} onClick={handleClose}>
-              {option}
-            </MenuItem>
-          ))}
-        </Menu>
-      </div>
+      <div className={classes.divider} />
       {userCardSetDatabase.map(userCardSet => (
         <EditDeckListItem
           key={userCardSet.id}
